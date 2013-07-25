dc7be3f82d1ad7e50c52e1d4b8d4446bbcaa83fe
R. Cohen <raphael.cohen@gmail.com>
2013-07-25 18:22:14 +0200

    infinite scroll

--- js/script.js
+++ js/script.js
@@ -2,46 +2,19 @@
 /*global $, alert*/
 var app = {
 		title: "Cats Pics",
-		version: "1.0.1.0",
+		version: "1.0.1.1",
 		tags: "cat",
-		moreButtonLabel: "More Cats &#187;",
 		flickrAPIKey: "b4bc32f4bec34c45463aa6c224e56e2e",
 		retryFrequency: 5000
 	},
 	lastPage = 0,
+	loadingImages = false,
 	flickrUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="
 		+ app.flickrAPIKey + "&tags=" + app.tags + "&per_page=10&extras=url_o&format=json";
-
-function onImgLoad(e) {
-	"use strict";
-	$(e.target).removeClass("imgPicsFadingIn").addClass("imgPics");
-}
-
-function jsonFlickrApi(data) {
-	"use strict";
-
-	var i, img;
-	if (data.stat === "ok" && data.photos.photo) {
-		for (i = 0; i < data.photos.photo.length; i += 1) {
-			if (data.photos.photo[i].farm && data.photos.photo[i].server
-					&& data.photos.photo[i].id && data.photos.photo[i].secret) {
-				img = $(document.createElement("img"));
-				img.addClass("imgPicsFadingIn");
-				img.bind("load", onImgLoad);
-				$("#divPics").append(img);
-				img.attr("src", "http://farm" + data.photos.photo[i].farm
-					+ ".staticflickr.com/" + data.photos.photo[i].server
-					+ "/" + data.photos.photo[i].id
-					+ "_" + data.photos.photo[i].secret + "_m.jpg");
-			}
-		}
-	}
-}
 
 function doneLoading() {
 	"use strict";
 	$("#imgLoad").css("display", "none");
-	$("#btnMore").css("display", "inline-block");
 	$("#btnRefresh").removeClass().addClass("headerButton");
 }
 
@@ -58,11 +31,42 @@ function loadFlickrPhotos() {
 	document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-function btnMoreClick(e) {
+function onImgLoad(e) {
 	"use strict";
-	$("#btnMore").css("display", "none");
-	$("#imgLoad").css("display", "inline-block");
-	loadFlickrPhotos();
+	$("#divPics").append($(e.target));
+	$(e.target).fadeIn();
+}
+
+function jsonFlickrApi(data) {
+	"use strict";
+	var i, img;
+	if (data.stat === "ok" && data.photos.photo) {
+		for (i = 0; i < data.photos.photo.length; i += 1) {
+			if (data.photos.photo[i].farm && data.photos.photo[i].server
+					&& data.photos.photo[i].id && data.photos.photo[i].secret) {
+				img = $(document.createElement("img"));
+				img.addClass("imgPics");
+				img.hide();
+				img.bind("load", onImgLoad);
+				img.attr("src", "http://farm" + data.photos.photo[i].farm
+					+ ".staticflickr.com/" + data.photos.photo[i].server
+					+ "/" + data.photos.photo[i].id
+					+ "_" + data.photos.photo[i].secret + "_m.jpg");
+			}
+		}
+		loadingImages = false;
+	} else {
+		window.setTimeout(loadFlickrPhotos, app.retryFrequency);
+	}
+}
+
+function getMore() {
+	"use strict";
+	if (!loadingImages) {
+		loadingImages = true;
+		$("#imgLoad").css("display", "inline-block");
+		loadFlickrPhotos();
+	}
 }
 
 function btnRefreshClick(e) {
@@ -70,7 +74,14 @@ function btnRefreshClick(e) {
 	$("#btnRefresh").removeClass().addClass("headerButtonPressed");
 	$("#divPics").empty();
 	lastPage = 0;
-	btnMoreClick(null);
+	getMore();
+}
+
+function onScroll(e) {
+	"use strict";
+	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 50) {
+		getMore();
+	}
 }
 
 function onLoad() {
@@ -78,20 +89,18 @@ function onLoad() {
 	try {
 		document.title = app.title;
 		$("#spanHeaderLabel").html(app.title);
-		$("#btnMore").html(app.moreButtonLabel);
-		$("#btnMore").bind("touchend", btnMoreClick);
-		$("#btnMore").bind("click", btnMoreClick);
-		$("#btnRefresh").bind("touchend", btnRefreshClick);
+		$("#btnRefresh").bind("touchstart", btnRefreshClick);
 		$("#btnRefresh").bind("click", btnRefreshClick);
+		$(window).bind("scroll", onScroll);
 
-		btnMoreClick(null);
+		getMore();
 	} catch (e1) {
 		alert("err onload: " + e1);
 	}
 }
 
-//document.addEventListener("deviceready", function () {
+document.addEventListener("deviceready", function () {
 	$(function () {
 		onLoad();
 	});
-//}, false);
+}, false);
